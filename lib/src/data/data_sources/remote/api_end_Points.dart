85217e3135a68a36f55c9d9ac7e2a14d8d8b0ada
Sahil <sahilkommu10@gmail.com>
2024-04-26 00:47:07 +0530

    Added below Changes 1)API client for get and post is completed

---
+++ lib/src/data/data_sources/remote/api_end_Points.dart
@@ -0,0 +1,9 @@
+class ApiEndPoints{
+  ApiEndPoints._();
+
+  static const String GET_TAGS="tags";
+  static const String ADD_TAGS="add-tags";
+  static const String POSTS="posts";
+  static const String CATEGORIES="categories";
+
+}
